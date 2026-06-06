e740e6a6d81644156ecdc9f663b9d875a0d44227
AizuruPRJCT <mirzadarmawan80@gmail.com>
2026-06-06 20:03:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 //OGURIIIII
-const oguri = document.getElementById('kuda');
+
+const firebscfg = {
+    databaseURL: "https://oguri-sneeze-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+firebase.initializeApp(firebscfg);
+const dbg = firebase.database().ref('click_counter');
+
+
+const oguri = document.getElementById('oguri');
 const sneeze = document.getElementById('proboro');
+const num_display = document.getElementById('num-count');
+
+let click_count = 0;
+
+
+dbg.on('value', (snapshot) => {
+    const data_in = snapshot.val();
+    if (data_in !== null) {
+        click_count = data_in;
+        num_display.toLocaleString();
+        num_display.innerText = data_in.toLocaleString();
+    } else {
+        num_display.innerText = "0";
+    }
+});
+
 oguri.addEventListener('click', () => {
+   
     sneeze.currentTime = 0;
     sneeze.play();
+  
+
+    dbg.set(click_count + 1);
+    oguri.classList.remove('animasi-blobbing');
+    void oguri.offsetWidth; 
     oguri.classList.add('animasi-blobbing');
-    setTimeout(() => {
-        oguri.classList.remove('animasi-blobbing');
-    }, 600);
 });
